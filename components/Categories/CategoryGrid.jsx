abfed31db4c778d6fa683daf45f32b35a8769b23
Greg-CS <gregor.gr20@gmail.com>
2023-05-31 11:40:34 -0400

    fixed mobile categories card

--- components/Categories/CategoryGrid.jsx
+++ components/Categories/CategoryGrid.jsx
@@ -5,7 +5,7 @@ export const CategoryGrid = ({ categories }) => {
   console.log(categories)
   return (
     <>
-      <div className="grid grid-cols-2 gap-60 md:gap-4 md:grid-cols-3 mt-1 p-5 overflow-auto justify-items-center">
+      <div className="grid grid-cols-1 gap-60 md:gap-4 md:grid-cols-3 mt-1 p-5 overflow-auto justify-items-center">
         {categories?.length > 0 &&
           categories.map((category) => (
             <CatBox key={category.id} {...category} />
